712a7a17871c6c393a48eff5045c929751e9f64d
thales santos <thalesabsantos@gmail.com>
2023-08-29 21:43:32 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BlueBarComponent } from './components/blue-bar/blue-bar.component';
 import { BaseImgComponent } from './components/base-img/base-img.component';
 import { ProductsBarComponent } from './components/products-bar/products-bar.component';
 import { ProductsBar2Component } from './components/products-bar2/products-bar2.component';
+import { CreditCardComponent } from './components/credit-card/credit-card.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ProductsBar2Component } from './components/products-bar2/products-bar2.
     BlueBarComponent,
     BaseImgComponent,
     ProductsBarComponent,
-    ProductsBar2Component
+    ProductsBar2Component,
+    CreditCardComponent
   ],
   imports: [
     BrowserModule,
